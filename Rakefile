20af40b948abc25bc2be29a84b62e5b033113248
Jason Toy <jtoy@jtoy.net>
2009-09-21 20:57:25 -0700

    add vlad support

---
+++ Rakefile
@@ -0,0 +1,6 @@
+begin
+  require "vlad"
+  Vlad.load(:app => nil, :scm => "git")
+rescue LoadError
+  # do nothing
+end
---
+++ config/deploy.rb
@@ -0,0 +1,4 @@
+set :application, "del2ip"
+set :repository,   "git@github.com:jtoy/del2ip.git"
+set :domain, ENV['DEL2IP_DOMAIN']
+set :deploy_to, "/usr/local/www/rails/#{application}
